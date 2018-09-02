ba992e7b1ddac6f00398ff6fb68ac4b4b0e9a328
Kent <kcponline@outlook.com>
2018-09-01 21:09:45 -0700

    add url module to simulate routing

--- server.js
+++ server.js
@@ -1,14 +1,67 @@
 //Lets require/import the HTTP module
 var http = require('http');
 
+//The url library allows us to parse parts of the request url.
+var url = require('url');
+
 //Lets define a port we want to listen to
 var PORT=8080;
 
 //We need a function which handles requests and send response
 // kp note that response.end may seem similar with console.log but
 // kp it's printing to the DOM instead of hidden in console
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+function handleRequest(req, res){
+
+  // Capturing the url the request is made to.
+  var url_parts = url.parse(req.url);
+
+  // When we visit different urls, the switch statement call on different functions.
+  switch (url_parts.pathname) {
+    case '/':
+      display_root(url_parts.pathname, req, res);
+      break;
+    case '/portfolio':
+      display_portfolio(url_parts.pathname, req, res);
+      break;
+    case '/edit':
+    // nodejs' equivalent to console.log but it didn't work... maybe because express server is not installed?
+      sys.puts("display edit");
+      break;
+    default:
+      display_404(url_parts.pathname, req, res);
+  }
+}
+
+// When we visit the 'http://localhost:8080/' path, this function is run.
+// kp - this simulates a webpage content
+function display_root(url, req, res) {
+  var myHTML = '<html>';
+  myHTML += '<body><h1>Home Page</h1>';
+  myHTML += "<a href='/portfolio'>Portfolio</a>";
+  myHTML += '</body></html>';
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.end(myHTML);
+}
+
+// When we visit the 'http://localhost:8080/portfolio' path, this function is run.
+function display_portfolio(url, req, res) {
+  var myHTML = '<html>';
+  myHTML += '<body><h1>My Portfolio</h1>';
+  myHTML += "<a href='/'>Go Home</a>";
+  myHTML += '</body></html>';
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.end(myHTML);
+}
+
+// When we visit any path that is not specifically defined, this function is run.
+function display_404(url, req, res) {
+  res.writeHead(404, {
+    'Content-Type': 'text/html'
+  });
+  res.write("<h1>404 Not Found </h1>");
+  res.end("The page you were looking for: " + url + " can not be found ");
 }
 
 //Create a server
