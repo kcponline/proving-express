f5a4ae87b03d20316f86c4ad2f32a6d614580002
Kent <kcponline@outlook.com>
2018-09-05 17:58:01 -0700

    add serve html file but error

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-//Lets require/import the HTTP module
+// Requiring dependencies
 var http = require('http');
+var fs = require('fs');
 
-//Lets define a port we want to listen to
-var PORT=8080;
+// Set our port to 80
+// kp port 80 is a mistake and the server won't start. it has to be 8080
+var PORT = 8080;
 
-//We need a function which handles requests and send response
-// kp note that response.end may seem similar with console.log but
-// kp it's printing to the DOM instead of hidden in console
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-// kp notice the use of the HTTP module, createServer method, and handleRequest function.
 var server = http.createServer(handleRequest);
 
-//Lets start our server
+function handleRequest(req, res){
+
+	fs.readFile("index.html", function(err, data){
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.end(data);
+	});
+
+};
+
+// Starts our server.
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+	console.log("Server is listening on PORT: " + PORT);
 });
+
+
